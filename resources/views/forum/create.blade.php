b5fc5421a2e436dc290b62503f2fadee73bdd1a1
Brilly <brillyanto.m@gmail.com>
2019-03-08 19:16:53 +0530

    Adding channel field on created thread form

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -13,17 +13,35 @@
                     @csrf
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input class="form-control" type="text" name="title" id="title">
-                            <input type="hidden" name="channel_id">
+                            <input class="form-control" value="{{old('title')}}" type="text" name="title" id="title">
                         </div>
+
+                        <div class="form-group">
+                            <label for="channel-id">Channel</label>
+                            <select class="form-control" name="channel_id" id="channel-id">
+                                <option value="">--Choose--</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option value="{{$channel->id}}" {{ $channel->id == old('channel_id') ? 'selected' : ''}} > {{$channel->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="body">Message</label>
                             <textarea class="form-control" name="body" id="body" cols="30" rows="10"></textarea>
                         </div>
                         
-                        <input type="submit" value="submit">
+                        <input class="btn btn-primary" type="submit" value="submit">
 
                     </form>   
+
+                    @if(count($errors))
+                    <ul class="alert-warning">
+                        @foreach($errors->all() as $error)
+                           <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                    @endif
                     
                 </div>
             </div>
