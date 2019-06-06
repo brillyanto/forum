771add24fa14bfe1569b241ca407b413f17053c7
Brilly <brillyanto.m@gmail.com>
2019-06-06 18:07:27 +0530

    Thread Subscription part 1

--- tests/Feature/UserParticipationTest.php
+++ tests/Feature/UserParticipationTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UserParticipationTest extends TestCase
 {
 
-    // use RefreshDatabase;
+    use RefreshDatabase;
 
     /**
      * A basic test example.
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -16,18 +16,18 @@ class ThreadTest extends TestCase
      *
      * @return void
      */
-    public function test_a_thread_have_many_replies()
+    function test_a_thread_have_many_replies()
     {
         $thread = factory('App\Thread')->create();
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
     }
 
-    public function test_a_thread_has_a_owner(){
+    function test_a_thread_has_a_owner(){
         $thread = factory('App\Thread')->create();
         $this->assertInstanceOf('App\User', $thread->author);
     }
 
-    public function test_a_thread_can_add_a_reply(){
+    function test_a_thread_can_add_a_reply(){
         $thread = factory('App\Thread')->create();
         $thread->addReply([
             'body' => 'Foobar',
@@ -37,10 +37,30 @@ class ThreadTest extends TestCase
         $this->assertCount(1, $thread->replies);
     }
 
-    public function test_a_thread_belongs_to_a_channel(){
+    function test_a_thread_belongs_to_a_channel(){
         $thread = factory('App\Thread')->create();
         $this->assertInstanceOf('App\Channel',$thread->channel);
     }
 
+    function test_a_thread_can_be_subscribed_to(){
+        $thread = factory('App\Thread')->create();
+        $user_id = 1;
+        $thread->subscribe($user_id);
+        // subscriptions is a relationship
+        $subscibers = $thread->subscriptions()->where('user_id', $user_id)->count();
+        $this->assertEquals(1, $subscibers);
+    }
+
+    function test_a_thread_can_be_unsubscribed_from(){
+        $thread = create('App\Thread');
+        $user_id = 1;
+
+        $thread->subscribe($user_id);
+        $thread->unsubscribe($user_id);
+
+        $subscribers = $thread->subscriptions()->where(['user_id' => $user_id])->count();
+        $this->assertEquals(0, $subscribers);
+
+    }
 
 }
